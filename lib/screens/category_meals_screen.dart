d274b0fe7ea8f6bdb30a89fd27b7523b21d0d45d
UMChannar <usamamuhammadchannar@gmail.com>
2023-03-09 13:57:16 +0500

    Dummy Data Showing

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../dummy_data.dart';
+import '../widgets/meal_item.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   static String routeName = '/category-meals';
@@ -29,8 +30,12 @@ class CategoriesMealsScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return Text(
-            categoryMeals[index].title,
+          return MealItem(
+            title: categoryMeals[index].title,
+            imageUrl: categoryMeals[index].imageUrl,
+            duation: categoryMeals[index].duration,
+            affordability: categoryMeals[index].affordability,
+            complexity: categoryMeals[index].complexity,
           );
         },
         itemCount: categoryMeals.length,
